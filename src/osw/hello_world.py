d7fe7d00b752be80a6521ac6ea39233ec7e3ac37
Matthias Koenig <konigmatt@googlemail.com>
2025-11-28 12:23:58 +0100

    changes for release

--- src/osw/hello_world.py
+++ src/osw/hello_world.py
@@ -2,7 +2,8 @@ from osw import console
 
 def hello_world() -> None:
     """Print greeting message."""
-    console.print("Hello World!", style="bold green")
+    console.print("Hello World!", style="bold red")
+    console.rule(style="white")
 
 
 if __name__ == "__main__":
